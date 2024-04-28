326f7dfc23cc0cd00e37b83e027bcdd1fe70c5e7
Peter Li <peterlimail47@gmail.com>
2024-04-27 20:17:10 -0700

    incompatible pointers

--- src/halc_allocators.h
+++ src/halc_allocators.h
@@ -51,7 +51,7 @@ errc setup_default_custom_allocator(void* (*malloc_fn) (size_t), void (*free_fn)
 // size of old allocation is needed for potential perf optimizations and  debugging
 //
 // modifies existing pointer, on failure no reallocation happens
-#define hrealloc(ptr, size, newSize, allowShrink) try(hrealloc_advanced(ptr, size, newSize, allowShrink, __FILE__, __LINE__, __func__))
+#define hrealloc(ptr, size, newSize, allowShrink) try(hrealloc_advanced((void**)ptr, size, newSize, allowShrink, __FILE__, __LINE__, __func__))
 
 // backing code for halloc
 errc halloc_advanced(void** ptr, size_t size, const char* file, i32 lineNumber, const char* func);
