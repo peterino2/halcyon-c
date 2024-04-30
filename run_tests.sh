7564adef3f05205fb3b6fe56d21f5f3d1f0b492e
Peter Li <peterlimail47@gmail.com>
2024-04-29 17:03:12 -0700

    adding -g

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,3 @@ cd build_debug;
 cmake -DCMAKE_BUILD_TYPE="Debug" ../ && cmake --build . && cd .. && ./build_debug/halcyon_test
 mkdir TestResults/;
 size build_debug/halcyon_test >> TestResults/binarySize
-
-mkdir build_debug;
-cd build_debug;
-cmake -DCMAKE_BUILD_TYPE="Release" ../ && cmake --build . && cd .. && ./build_debug/halcyon_test
-mkdir TestResults/;
-size build_debug/halcyon_test >> TestResults/size
